7672b19c4b4b2fde8e67a62681e5dd49192c1a16
alguemqualquer123 <danilovinicius10337@gmail.com>
2026-05-15 18:04:47 -0300

    'a'

---
+++ assets/images/sounds/index.ts
@@ -0,0 +1,13 @@
+import alarm from './alarm.ogg';
+import dial from './dial.mp3';
+import notification from './notification.mp3';
+import photo from './photo.ogg';
+import ring from './ring.mp3';
+
+export {
+    alarm,
+    dial,
+    notification,
+    photo,
+    ring,
+}
